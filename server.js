fe12b256b4d57a6b3321e57bb98706589fd8c648
Maazii <maaz_rehman39@hotmail.com>
2023-10-18 14:27:45 +0500

    Adding nodemon

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-const os = require("os");
-const mathFunc = require("./math");
-
-// Common Core Modules
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
-
-mathFunc.add(7, 15);
+console.log("This is changed!!");
