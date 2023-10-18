9567c2ab2e87164bcb9d5ef96338e455a3002695
Maazii <maaz_rehman39@hotmail.com>
2023-10-18 14:17:31 +0500

    Starter

---
+++ math.js
@@ -0,0 +1,11 @@
+const add = (number1, number2) => {
+    const result = number1 + number2;
+    console.log(result);
+}
+
+const multiply = (number1, number2) => {
+    const result = number1 * number2;
+    console.log(result);
+}
+
+module.exports = { add, multiply };
---
+++ server.js
@@ -0,0 +1,9 @@
+const os = require("os");
+const mathFunc = require("./math");
+
+// Common Core Modules
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+mathFunc.add(7, 15);
