c8caec728ce97bc6684ddd10efea4eac403cee36
Maazii <maaz_rehman39@hotmail.com>
2024-01-06 11:08:02 +0500

    Testing Jenkins

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    // res.sendFile("./views/index.html", {root: __dirname}); One way to serve a file.
-    res.sendFile(path.join(__dirname, "views", "index.html"));
-})
+app.get("^/$|/index(.html)?", (req, res) => {
+  // res.sendFile("./views/index.html", {root: __dirname}); One way to serve a file.
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-})
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
